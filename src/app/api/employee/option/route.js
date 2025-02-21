be275fece21a922e1c6f479b5077f27e1fe5e751
kxinyin <132971009+kxinyin@users.noreply.github.com>
2025-02-21 18:34:48 +0800

    feat: add employee option api

---
+++ src/app/api/employee/option/route.js
@@ -0,0 +1,30 @@
+import { connectToDatabase } from "@/src/lib/mongodb";
+
+export async function GET(request) {
+  const searchParams = request.nextUrl.searchParams;
+  const role_id = searchParams.get("role_id");
+
+  const { db } = await connectToDatabase();
+
+  const FILTER = { is_enabled: true, ...(role_id && { role_id }) };
+
+  const employees = await db
+    .collection("employees")
+    .find(FILTER, { projection: { _id: 0, employee_id: 1, username: 1 } })
+    .toArray();
+
+  if (!employees) {
+    return new Response(
+      JSON.stringify({ message: "No employees found", data: null }),
+      { status: 404 }
+    );
+  }
+
+  const message = role_id
+    ? `Successfully retrieved all employees with role: ${role_id}`
+    : "Successfully retrieved all employees";
+
+  return new Response(JSON.stringify({ message, data: employees }), {
+    status: 200,
+  });
+}
